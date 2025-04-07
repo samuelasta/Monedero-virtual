9e90c1c7d36bfe8e42ad0eeb7175ba3759b6ba1b
samuelasta <samuela.castanom@uqvirtual.edu.co>
2025-04-07 11:14:10 -0500

    estructura del proyecto

---
+++ mi-moneda/src/main/java/co/uniquindio/edu/mi_moneda/MiMonedaApplication.java
@@ -0,0 +1,13 @@
+package co.uniquindio.edu.mi_moneda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiMonedaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiMonedaApplication.class, args);
+	}
+
+}
---
+++ mi-moneda/src/test/java/co/uniquindio/edu/mi_moneda/MiMonedaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uniquindio.edu.mi_moneda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiMonedaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
